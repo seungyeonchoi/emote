bf5d387a344a49f17a97b9cb086186cf7b096305
seungyeonchoi <48347010+seungyeonchoi@users.noreply.github.com>
2019-05-20 01:17:17 +0900

    Delete MyDust.kt

--- a190306app/MyDust.kt
+++
@@ -1,9 +0,0 @@
-package com.example.a190306app
-/*
-* pm10: 미세먼지 농도
-* pm25: 초미세먼지 농도
-* idex_nm: 통합대기환경등급 ex. 좋음, 보통 , ...
-* idex_mval: 통합대기환경지수
-* */
-data class MyDust(val pm10:Double, val pm25:Double, val idex_nm:String,val idex_mvl:Double) {
-}
