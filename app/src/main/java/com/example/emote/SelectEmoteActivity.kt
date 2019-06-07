358dabfcecfb3eddada7c349a9f62600efafc6a5
nari <hr_babn@naver.com>
2019-06-08 00:22:34 +0900

    narizzang

---
+++ app/src/main/java/com/example/emote/SelectEmoteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.emote
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectEmoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_emote)
+    }
+}
