bb8d1bd2918933a3c655156b431e5b08a73d2c78
san9640 <44027624+san9640@users.noreply.github.com>
2019-06-12 17:21:43 +0900

    로그인 완성 - 황
    Intent의 putExtra로 키값이 "uid", 저장 값이 로그인한 사용자의 uid를 넣음
    MainActivity에서
    if(intent.hasExtra("uid")){
        val crt_uid = getStringExtra("uid")
    }
    를 사용하면 crt_uid에 로그인한사람의 uid 저장됨

--- app/src/main/java/com/example/emote/LoginActivity.kt
+++ app/src/main/java/com/example/emote/LoginActivity.kt
@@ -13,12 +13,26 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun OnBtnClick(view: View) {
-        when(view.id){
-            R.id.btn_login->{
-                startActivity(Intent(this,MainActivity::class.java)) //selectemote확인해보셈 ~!~!
+        when (view.id) {
+            R.id.btn_login -> {
+                val id_str = log_id.text.toString()
+                val pw_str = log_pw.text.toString()
+
+                val user = DB().getUsersByQuery("tel = $id_str")
+                if(user?.size!! == 0) {
+                    Toast.makeText(this, "아이디가 일치하지 않습니다.", Toast.LENGTH_LONG).show()
+                }else if(user[0].pw != pw_str) {
+                    Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG).show()
+                }else {
+                    Toast.makeText(this, "로그인 되었습니다.", Toast.LENGTH_LONG).show()
+                    val uid = user[0].uid // 전역 변수로 넘겨주기 가능?
+                    val nextIntent = Intent(this, MainActivity::class.java)
+                    nextIntent.putExtra("uid", uid)
+                    startActivity(nextIntent) //selectemote확인해보셈 ~!~!
+                }
             }
-            R.id.btn_join1->{
-                startActivity(Intent(this,SigninActivity::class.java))
+            R.id.btn_join1 -> {
+                startActivity(Intent(this, SigninActivity::class.java))
             }
         }
     }
